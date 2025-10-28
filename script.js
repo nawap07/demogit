b3ea4d0aec106387f7ba10a5243b4d6e75f21aaf
nawap07 <pawanmahatoop07@gmail.com>
2025-10-28 18:08:27 +0530

    add prompt

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Good Evening Pawan");
+
+const number = prompt("Please enter a number");
+
+for (let i = 1; i <= 10; i++) {
+    let res = number * i;
+    console.log(number);
+}
+
+let num = 5;
+for (let i = 1; i <= num; i++) {
+    let star = "";
+    for (let j = 1; j <= num - i; j++) {
+        star = "*"
+    }
+    console.log(star);
+}
