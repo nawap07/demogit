7c00a06016af9830d296911f5b0f9e53e3ab253d
nawap07 <pawanmahatoop07@gmail.com>
2025-10-31 21:03:34 +0530

    Add clousers

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
 console.log("Good Evening Pawan");
 
-const number = prompt("Please enter a number");
+// const number = prompt("Please enter a number");
 
-for (let i = 1; i <= 10; i++) {
-    let res = number * i;
-    console.log(number);
-}
+// for (let i = 1; i <= 10; i++) {
+//     let res = number * i;
+//     console.log(number);
+// }
 
-let num = 5;
-for (let i = 1; i <= num; i++) {
-    let star = "";
-    for (let j = 1; j <= num - i; j++) {
-        star = "*"
-    }
-    console.log(star);
-}
+// let num = 5;
+// for (let i = 1; i <= num; i++) {
+//     let star = "";
+//     for (let j = 1; j <= num - i; j++) {
+//         star = "*"
+//     }
+//     console.log(star);
+// }
 
 //Clousers in js 
-function abcd(){
-    let a =12;
-    return function(){
+function abcd() {
+    let a = 12;
+    return function () {
         console.log(a);
     }
 }
 
+//Create a Toaster notifications
+
+function toaster(config) {
+    return function (str) {
+        const div = document.createElement("div");
+        div.className = ``;
+        div.textContent = str;
+        document.querySelector(".parent").appendChild(div)
+        setTimeout(() => { 
+            document.querySelector(".parent").removeChild(div)
+        }, config.duration * 1000);
+    }
+
+}
+
+let toasterData = toaster({
+    positionX: 'right',
+    positionY: 'top',
+    theme: 'dark',
+    duration: 3
+})
+
+toasterData("Good morning pawan")
