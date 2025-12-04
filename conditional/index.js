76c7ee0b2530e897cf8bd553e44fe571723faa44
nawap07 <pawanmahatoop07@gmail.com>
2025-12-04 17:45:33 +0530

    Add initial HTML and JavaScript files for shop discount and movie ticket pricing

---
+++ conditional/index.js
@@ -0,0 +1,32 @@
+// Shop Discount 
+
+// let price = Number(prompt("Pay Here"));  //0-3000 -0% || 3001-5000 -10% || 5001 - 9000 -15%  || 9000 - 20%
+
+// if (price >= 0 && price <= 3000) {
+//     console.log("You Have to Pay  = ", price);
+// } else if (price >= 3001 && price <= 5000) {
+//     console.log("You Have To Pay = ", price - Math.floor((price * 10) / 100));
+// } else if (price >= 5001 && price < 9000) {
+//     console.log("You Have to pay = ", price - Math.floor((price * 15) / 100));
+// } else {
+//     console.log("You Have to pay = ", price - Math.floor((price * 20) / 100));
+// }
+
+
+//Movie Ticket Price
+
+// let age = Number(prompt("Enter Your Age"));
+
+// if(age){
+//     if (age <= 10) {
+//     console.log("Your Ticket Price is 100");
+// } else if (age <= 17) {
+//     console.log("Your Ticket Price is 150");
+// } else if (age >= 18) {
+//     console.log("Your Ticket Price is 200");
+// } else {
+//     console.log("Please enter age");
+// }
+// }else{
+//     console.log("Enter age");
+// }
